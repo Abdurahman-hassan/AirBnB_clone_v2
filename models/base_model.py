0206f6ab1c5589596b56d4adaf2b3bd7a4ffc204
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-21 23:02:55 +0200

    undo linting

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Provides a base class for data models with common functionality."""
 import uuid
 from datetime import datetime
+
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import models
@@ -16,9 +17,9 @@ else:
 class BaseModel:
     """Base class for data models."""
     if models.storage_type == "db":
-        id = Column(String(60), primary_key=True, nullable=False)
-        created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-        updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """
@@ -28,7 +29,6 @@ class BaseModel:
             *args: Variable length argument list.
             **kwargs: Arbitrary keyword arguments.
         """
-
         if kwargs:
             # The __class__ key if being deleted from the kwargs
             # in the BaseModel __init__ method without first checking
@@ -46,8 +46,8 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -62,7 +62,7 @@ class BaseModel:
         """Save the current state of the object and trigger
         the storage save process.
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
@@ -70,11 +70,9 @@ class BaseModel:
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
-            new_dict["created_at"] = \
-                new_dict["created_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")
+            new_dict["created_at"] = new_dict["created_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")
         if "updated_at" in new_dict:
-            new_dict["updated_at"] = \
-                new_dict["updated_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")
+            new_dict["updated_at"] = new_dict["updated_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
